8ee16e8efaeb7eb38c3066e0eb9888f554017c67
Phu Tang <phutanghong@gmail.com>
2018-09-05 16:28:52 +0700

    add contract to index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import * as common from './common';
 import provider from './provider';
 import Api from './api';
+import contract from './contract';
 
 export default url => ({
   provider: provider(url),
   api: Api(provider(url)),
-  common: common
+  common: common,
+  contract: contract
 });
